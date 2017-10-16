4420baa76a8b1e574d92196fe56b6ad998718ab6
jstass <jasonstass@gmail.com>
2017-10-15 21:07:25 -0500

    Updated home page and Css stuff

---
+++ js/index.js
@@ -0,0 +1,33 @@
+// JavaScript Document
+var products =[
+	{
+		"image" : "images/product1.png"
+	},
+	{
+		"image" : "images/product2.png"
+	},
+	{
+		"image" : "images/product3.png"
+	},
+	{
+		"image" : "images/product4.png"
+	},
+	{
+		"image" : "images/product5.png"
+	},
+	{
+		"image" : "images/product6.png"
+	},
+	{
+		"image" : "images/product7.png"
+	},
+	{
+		"image" : "images/product8.png"
+	}
+];
+
+function setUpPage{
+	
+}
+
+window.addEventListener("load", setUpPage, false);
